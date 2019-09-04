26c571692b0e91b77c4e94be03732f4a0c620cb3
Leonardo <leonardo.capoccia@hotmail.it>
2019-09-04 17:40:26 +0200

    Add names for document collections

--- src/main/java/it/unisalento/tripplanner/model/ItineraryModel.java
+++ src/main/java/it/unisalento/tripplanner/model/ItineraryModel.java
@@ -1,7 +1,10 @@
 package it.unisalento.tripplanner.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.List;
 
+@Document(collection = "itinerary")
 public class ItineraryModel extends ExperienceModel {
 
     private List<TripStopModel> stops;
--- src/main/java/it/unisalento/tripplanner/model/TripModel.java
+++ src/main/java/it/unisalento/tripplanner/model/TripModel.java
@@ -1,7 +1,10 @@
 package it.unisalento.tripplanner.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.List;
 
+@Document(collection = "trip")
 public class TripModel extends ExperienceModel {
 
     private List<DayBagModel> dayBags;
