4d7f94eefe5db21eb91a2ab2d3527ba95cef2d14
Leonardo <leonardo.capoccia@hotmail.it>
2019-09-06 13:20:35 +0200

    Add PageBuilder

---
+++ src/main/java/it/unisalento/tripplanner/utils/PageBuilder.java
@@ -0,0 +1,40 @@
+package it.unisalento.tripplanner.utils;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
+
+@Singleton @Component
+public class PageBuilder<T> {
+
+    private List<T> elements = new ArrayList<>();
+    private Pageable pageRequest;
+    private int totalElements;
+
+    public PageBuilder() {
+    }
+
+    public PageBuilder<T> setElements(List<T> elements) {
+        this.elements = elements;
+        return this;
+    }
+
+    public PageBuilder<T> setPage(Pageable page) {
+        this.pageRequest = page;
+        return this;
+    }
+
+    public PageBuilder<T> setTotalElements(int numberOfElements) {
+        this.totalElements = numberOfElements;
+        return this;
+    }
+
+    public Page<T> build() {
+        return new PageImpl<>(this.elements, this.pageRequest, this.totalElements);
+    }
+}
