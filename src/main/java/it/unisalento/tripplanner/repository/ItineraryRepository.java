8c373f7f98240122de5555f05cbf441363f47b27
Leonardo <leonardo.capoccia@hotmail.it>
2019-09-09 13:52:20 +0200

    Fix: repository annotation to Itinerary repository

--- src/main/java/it/unisalento/tripplanner/repository/ItineraryRepository.java
+++ src/main/java/it/unisalento/tripplanner/repository/ItineraryRepository.java
@@ -4,7 +4,9 @@ import it.unisalento.tripplanner.model.ItineraryModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItineraryRepository extends MongoRepository<ItineraryModel, String> {
     Page<ItineraryModel> findByUserId(String userID, Pageable pageRequest);
 }
